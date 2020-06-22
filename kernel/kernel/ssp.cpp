448c88898d59e2668aea8869cb9c4f4f7db39135
alexandre <aran.alexandre@gmail.com>
2020-06-23 00:15:29 +0200

    Add simple stack smashing protector
    Following https://wiki.osdev.org/Stack_Smashing_Protector
    We don't really need random guard since the problems to detect are accidental and not malicious at least for now

---
+++ kernel/kernel/ssp.cpp
@@ -0,0 +1,27 @@
+#include <stdint.h>
+
+#if __STDC_HOSTED__
+#include <stdlib.h>
+#elif __is_kernel
+#include "kernel/terminal.hpp"
+#endif
+
+#if UINT32_MAX == UINTPTR_MAX
+#define STACK_CHK_GUARD 0xe2dee396
+#else
+#define STACK_CHK_GUARD 0x595e9fbd94fda766
+#endif
+
+uintptr_t __stack_chk_guard = STACK_CHK_GUARD;
+
+
+extern "C" __attribute__((noreturn)) void __stack_chk_fail(void)
+{
+#if __STDC_HOSTED__
+    abort();
+#elif __is_kernel
+    Terminal terminal;
+    terminal.writeString("Stack smashing detected");
+    while(1);
+#endif
+}
