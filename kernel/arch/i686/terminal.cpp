db2bb4cb16515fa5d5afa243a894b15349a15ab7
alexandre <aran.alexandre@gmail.com>
2020-06-24 15:40:06 +0200

    Update printf

--- kernel/arch/i686/terminal.cpp
+++ kernel/arch/i686/terminal.cpp
@@ -7,111 +7,180 @@ static uint16_t* const VGA_MEMORY{reinterpret_cast<uint16_t*>(0xB8000)};
 
 size_t strlen(const char* str)
 {
-        size_t len = 0;
-        while (str[len])
-                len++;
-        return len;
+    size_t len = 0;
+    while (str[len])
+        len++;
+    return len;
 }
 
 int Terminal::printf(const char* format, ...)
 {
-        va_list parameters;
-        va_start(parameters, format);
+    va_list parameters;
+    va_start(parameters, format);
 
-        while (*format != '\0')
+    while (*format != '\0')
+    {
+        if (format[0] != '%' || format[1] == '%')
         {
-                if (format[0] != '%' || format[1] == '%')
-                {
-                        if (format[0] == '%')
-                                format++;
-                        size_t amount = 1;
-                        while (format[amount] && format[amount] != '%')
-                                amount++;
+            if (format[0] == '%')
+                format++;
+            size_t amount = 1;
+            while (format[amount] && format[amount] != '%')
+                amount++;
 
-                        write(format, amount);
-                        format += amount;
-                        continue;
-                }
-
-                const char* format_begun_at = format++;
-
-                if (*format == 'c')
-                {
-                        format++;
-                        char c = (char) va_arg(parameters, int /* char promotes to int */);
-                        write(&c, sizeof(c));
-                } else if (*format == 'u') {
-                        format++;
-                        int i = va_arg(parameters, int);
-                        char str[30];
-                        int len{0};
-                        for(len=0;i!=0; i/=10)
-                        {
-                            char n=i%10;
-                            len++;
-                            str[30-len]='0'+n;
-                        }
-                        if(len!=0)
-                            write(&(str[30-len]), len);
-                        else
-                            putChar('0');
-                } else if (*format == 'b') {
-                    format++;
-                    int i = va_arg(parameters, int);
-                    char str[32];
-                    int len{0};
-                    for(len=0;i!=0; i/=2)
-                    {
-                        char n=i%2;
-                        len++;
-                        str[32-len]='0'+n;
-                    }
-                    if(len!=0)
-                        write(&(str[32-len]), len);
-                    else
-                        putChar('0');
-                } else if (*format == 'x') {
-                    format++;
-                        uint32_t i = va_arg(parameters, uint32_t);
-                        char str[30];
-                        int len{0};
-                        for(len=0;i!=0; i/=16)
-                        {
-                            char n=i%16;
-                            len++;
-                            str[30-len]=(n<10)?'0'+n:'a'-10+n;
-                        }
-                        if(len!=0)
-                            write(&(str[30-len]), len);
-                        else
-                            putChar('0');
-                } else if (*format == 'p') {
-                    format++;
-                    uint32_t i = va_arg(parameters, uint32_t);
-                    char str[8];
-                    int len{0};
-                    for(len=0;len<8; i/=16)
-                    {
-                        char n=i%16;
-                        len++;
-                        str[8-len]=(n<10)?'0'+n:'a'-10+n;
-                    }
-                    write(&(str[8-len]), len);
-                } else if (*format == 's') {
-                    format++;
-                    const char* str = va_arg(parameters, const char*);
-                    size_t len = strlen(str);
-                    write(str, len);
-                } else {
-                    format = format_begun_at;
-                    size_t len = strlen(format);
-                    write(format, len);
-                    format += len;
-                }
+            write(format, amount);
+            format += amount;
+            continue;
         }
 
-        va_end(parameters);
-        return 0;
+        const char* format_begun_at = format++;
+
+        if (*format == 'c')
+        {
+            format++;
+            char c = (char) va_arg(parameters, int /* char promotes to int */);
+            write(&c, sizeof(c));
+        } else if (*format == 'u') {
+            format++;
+            uint32_t i = va_arg(parameters, uint32_t);
+            char str[30];
+            int len{0};
+            for(len=0;i!=0; i/=10)
+            {
+                char n=i%10;
+                len++;
+                str[30-len]='0'+n;
+            }
+            if(len!=0)
+                write(&(str[30-len]), len);
+            else
+                putChar('0');
+        } else if (*format == 'b') {
+            format++;
+            uint32_t i = va_arg(parameters, uint32_t);
+            char str[128];
+            int len{0};
+            for(len=0;i!=0; i/=2)
+            {
+                char n=i%2;
+                len++;
+                str[128-len]='0'+n;
+            }
+            if(len!=0)
+                write(&(str[128-len]), len);
+            else
+                putChar('0');
+        } else if (*format == 'x') {
+            format++;
+            uint32_t i = va_arg(parameters, uint32_t);
+            char str[30];
+            int len{0};
+            for(len=0;i!=0; i/=16)
+            {
+                char n=i%16;
+                len++;
+                str[30-len]=(n<10)?'0'+n:'a'-10+n;
+            }
+            if(len!=0)
+                write(&(str[30-len]), len);
+            else
+                putChar('0');
+        } else if (*format == 'p') {
+            format++;
+            uint32_t i = va_arg(parameters, uint32_t);
+            char str[32];
+            int len{0};
+            for(len=0;len%8!=0||i!=0; i/=16)
+            {
+                char n=i%16;
+                len++;
+                str[32-len]=(n<10)?'0'+n:'a'-10+n;
+            }
+            if(len!=0)
+                write(&(str[32-len]), len);
+            else
+                putChar('0');
+        }
+        else if (*format == 'c')
+        {
+            format++;
+            char c = (char) va_arg(parameters, int /* char promotes to int */);
+            write(&c, sizeof(c));
+        } else if (*format == 'U') {
+            format++;
+            uint64_t i = va_arg(parameters, uint64_t);
+            char str[30];
+            int len{0};
+            for(len=0;i!=0; i/=10)
+            {
+                char n=i%10;
+                len++;
+                str[30-len]='0'+n;
+            }
+            if(len!=0)
+                write(&(str[30-len]), len);
+            else
+                putChar('0');
+        } else if (*format == 'B') {
+            format++;
+            uint64_t i = va_arg(parameters, uint64_t);
+            char str[128];
+            int len{0};
+            for(len=0;i!=0; i/=2)
+            {
+                char n=i%2;
+                len++;
+                str[128-len]='0'+n;
+            }
+            if(len!=0)
+                write(&(str[128-len]), len);
+            else
+                putChar('0');
+        } else if (*format == 'X') {
+            format++;
+            uint64_t i = va_arg(parameters, uint64_t);
+            char str[30];
+            int len{0};
+            for(len=0;i!=0; i/=16)
+            {
+                char n=i%16;
+                len++;
+                str[30-len]=(n<10)?'0'+n:'a'-10+n;
+            }
+            if(len!=0)
+                write(&(str[30-len]), len);
+            else
+                putChar('0');
+        } else if (*format == 'P') {
+            format++;
+            uint64_t i = va_arg(parameters, uint64_t);
+            char str[32];
+            int len{0};
+            for(len=0;len%8!=0||i!=0; i/=16)
+            {
+                char n=i%16;
+                len++;
+                str[32-len]=(n<10)?'0'+n:'a'-10+n;
+            }
+            if(len!=0)
+                write(&(str[32-len]), len);
+            else
+                putChar('0');
+        } else if (*format == 's') {
+            format++;
+            const char* str = va_arg(parameters, const char*);
+            size_t len = strlen(str);
+            write(str, len);
+        } else {
+            format = format_begun_at;
+            size_t len = strlen(format);
+            write(format, len);
+            format += len;
+        }
+    }
+
+    va_end(parameters);
+    return 0;
 }
 
 
